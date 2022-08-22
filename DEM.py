59450c3e5f60e4807833e9423b93755bc1c109a1
vdvorian <vdvorian1@mail.ru>
2022-08-22 23:37:08 +0300

    + DEM

--- DEM.py
+++ DEM.py
@@ -1,9 +1,9 @@
 import scipy.integrate as integrate
 
 
-# вспомогательная функция для integrate.solve_ivp: подготовка ОДУ
-def prepare_ODU_function(t, y):
-    k1, n1, k2, n2 = y
+def prepare_ODE_function(t, y):
+    k1, n1 = y
+    k2, n2 = 0, 0
 
     p = (k1 + 4 / 3 * n1) / (k2 + 4 / 3 * n1)
     dzeta = n1 * (9 * k1 + 8 * n1) / (6 * (k1 + 2 * n1))
@@ -15,11 +15,10 @@ def prepare_ODU_function(t, y):
     return [dkdt, dndt]
 
 
-
-def get_all_values_by_DEM(matrix, pores):
+def get_all_values_by_DEM(matrix):
     # функция принимает на вход два массива следующего вида:
     # pores = [bulk_p, shear_p], где элементы массива: bulk и shear модули и аспектное отношение флюида,
-        # насыщающего поры (воздух, например)
+    # насыщающего поры (воздух, например)
     # matrix = [bulk_m, shear_m]
 
     # porosity - пористость
@@ -27,11 +26,12 @@ def get_all_values_by_DEM(matrix, pores):
     # возвращает bulk и shear модули композита матрица+поры
 
     k1, n1 = matrix
-    k2, n2 = pores
 
-
-    result = integrate.solve_ivp(fun = prepare_ODU_function, t_span = (0,0.999), y0 = [k1, n1],
+    result = integrate.solve_ivp(fun = prepare_ODE_function, t_span = (0, 0.999), y0 = [k1, n1],
                                  dense_output = 'true', max_step = 0.001, vectorized = 'true')
 
-    return result.t, result.y[0], result.y[1]
+    por = result.t
+    k_dem = result.y[0]
+    n_dem = result.y[1]
 
+    return por, k_dem, n_dem
